cbb05da3f65d5e6a8ff81a4317cfffc88ce469b5
nirvair09 <chandradeep262003@gmail.com>
2025-10-28 19:19:12 +0530

    rate limit logic implemented and working fine ,but needs refinement

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-express=require('express');
+express = require('express');
+const ip = require('ip');
 
-const app=express();
+const app = express();
 
-app.listen(9000,()=>{
-    console.log("App is running on port 9000");
+
+const MAX_REQ_LIMIT = 10;
+const MAX_TIME_WINDOW = 60_000; // 1 minute in terms of miliseconds
+
+const requestCount = {};
+
+app.use((req, res, next) => {
+    const clientIP = req.ip;
+
+    if (!requestCount[clientIP]) {
+        requestCount[clientIP] = 1;
+    } else {
+        requestCount[clientIP]++;
+    }
+
+    if (requestCount[clientIP] > MAX_REQ_LIMIT) {
+        return res.status(429).send("Too many requests , you hit the rate limitter , kindly try after 1min");
+
+    } else {
+        next();
+    }
+
 })
 
+setInterval(() => {
+    for (const id in requestCount) {
+        requestCount[id] = 0;
+    }
+}, MAX_TIME_WINDOW)
 
-app.get('/',(req,res)=>{
+
+
+app.get('/', (req, res) => {
     console.log("Request received");
     res.status(200).send("Hello from rate limiter in nodejs");
-})
+});
+
+app.listen(9000, () => {
+    console.log("App is running on port 9000");
+    console.log(`my ip adress is ${ip.address()}`);
+});
+
+
